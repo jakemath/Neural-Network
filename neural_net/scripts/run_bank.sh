16807d44b828b64bf6d08a8d23ce070aaba08ae3
Jake Mathai <jakemathai5@gmail.com>
2020-04-30 21:00:54 -0700

    Update run_bank.sh

--- neural_net/scripts/run_bank.sh
+++ neural_net/scripts/run_bank.sh
@@ -1,5 +1,5 @@
-rm costs/bank_cost.txt
-cd ~/desktop/programming/c++/neural_net/neural_net/source
+cd .. && costs/bank_cost.txt
+cd source
 g++ -std=c++2a -c neuron.h layer.cpp layer.h dataset_utils.h dataset_utils.cpp neural_network.h neural_network.cpp
 g++ -std=c++2a -c main/bank_main.cpp
 g++ -o run_bank bank_main.o layer.o dataset_utils.o neural_network.o
