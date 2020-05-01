c800225e01ee071ccc696633ddf3a33278748ba3
Jake Mathai <jakemathai5@gmail.com>
2020-04-30 21:01:37 -0700

    Update run_max_index.sh

--- neural_net/scripts/run_max_index.sh
+++ neural_net/scripts/run_max_index.sh
@@ -1,7 +1,6 @@
-rm costs/max_index_cost.txt
-cd ~/desktop/programming/c++/neural_net/neural_net/source
+cd .. && rm costs/max_index_cost.txt
+cd source
 g++ -std=c++2a -c neuron.h layer.cpp layer.h dataset_utils.h dataset_utils.cpp neural_network.h neural_network.cpp
 g++ -std=c++2a -c main/max_index_main.cpp
 g++ -o run_max_index max_index_main.o layer.o dataset_utils.o neural_network.o
 ./run_max_index
-
